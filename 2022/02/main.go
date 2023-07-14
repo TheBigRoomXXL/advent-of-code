e46c29cab3066dbbbaf34bc7aa646531ee1a04b1
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-07-14 15:58:47 +0200

    2022-02

--- 2022/02/main.go
+++ 2022/02/main.go
@@ -1,11 +1,73 @@
 package main
 
-func check(e error) {
-	if e != nil {
-		panic(e)
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	part := flag.Int("part", 0, "part number (1 or 2)")
+	flag.Parse()
+	switch *part {
+	case 1:
+		part1()
+	case 2:
+		part2()
+	default:
+		panic("invalid part")
 	}
 }
 
-func main() {
+func part1() {
+	scores := map[string]int{
+		"A X": 4,
+		"A Y": 8,
+		"A Z": 3,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 7,
+		"C Y": 2,
+		"C Z": 6,
+	}
+
+	result := 0
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		result += scores[scanner.Text()]
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
+}
+
+func part2() {
+	scores := map[string]int{
+		"A X": 3,
+		"A Y": 4,
+		"A Z": 8,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 2,
+		"C Y": 6,
+		"C Z": 7,
+	}
+
+	result := 0
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		result += scores[scanner.Text()]
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println(result)
 
 }
---
+++ READEME.md
@@ -0,0 +1,6 @@
+# Advent of code
+
+Leaning the basics of go with advent of code.
+
+For more information on what is advent of code checkout the official site:
+[adventofcode.com](https://adventofcode.com/)
