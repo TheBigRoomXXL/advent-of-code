bfa9666bc3cacd2502c9049af26476636dce80a3
TheBigRoomXXL <seb.lovergne@gmail.com>
2023-07-15 17:05:00 +0200

    2022-01: forgot part2!

--- 2022/01/main.go
+++ 2022/01/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 )
 
@@ -14,6 +16,38 @@ func check(e error) {
 }
 
 func main() {
+
+	part := flag.Int("part", 0, "part number (1 or 2)")
+	flag.Parse()
+	switch *part {
+	case 1:
+		part1()
+	case 2:
+		part2()
+	default:
+		panic("invalid part")
+	}
+}
+
+func part1() {
+	elves := parseInpout()
+	max := 0
+	for i := 0; i <= len(elves)-1; i++ {
+		if elves[i] > max {
+			max = elves[i]
+		}
+	}
+
+	fmt.Println(max)
+}
+
+func part2() {
+	elves := parseInpout()
+	sort.Ints(elves)
+	fmt.Println(elves[len(elves)-1] + elves[len(elves)-2] + elves[len(elves)-3])
+}
+
+func parseInpout() []int {
 	elves := []int{0}
 
 	scanner := bufio.NewScanner(os.Stdin)
@@ -27,17 +61,5 @@ func main() {
 			elves = append(elves, 0)
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		check(err)
-	}
-
-	max := 0
-	for i := 0; i <= len(elves)-1; i++ {
-		if elves[i] > max {
-			max = elves[i]
-		}
-	}
-
-	fmt.Println(max)
-
+	return elves
 }
